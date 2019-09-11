dc1699f7194ff0fb83691c9dc4f46d46ead1bd47
Thokozani XABA <txaba@c5r3s9.wethinkcode.co.za>
2019-09-11 17:28:50 +0200

    new

---
+++ libft/ft_strcmp.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmuzeren <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/21 09:54:02 by tmuzeren          #+#    #+#             */
+/*   Updated: 2019/05/29 17:04:55 by tmuzeren         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	unsigned int	i;
+	unsigned char	*str;
+	unsigned char	*str2;
+
+	if (s1 == NULL || s2 == NULL)
+		return (-1);
+	i = 0;
+	str = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
+	while (str[i] && str2[i] && str[i] == str2[i])
+		i++;
+	return (str[i] - str2[i]);
+}
